96b96d5086e21955001cd862fa032aa20f631b57
Mark <markt@outlook.dk>
2025-01-27 19:28:17 +0100

    Add project files.

---
+++ WindowsAuthenticationExample/WindowsAuthenticationExample/Program.cs
@@ -0,0 +1,32 @@
+using WindowsAuthenticationExample.Client.Pages;
+using WindowsAuthenticationExample.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(WindowsAuthenticationExample.Client._Imports).Assembly);
+
+app.Run();
